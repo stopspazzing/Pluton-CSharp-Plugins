38336e02cbcb7e5f73aed9092662a76fc6903ad6
Jeremy Zimm <stopspazzing@gmail.com>
2015-07-08 14:08:59 -0700

    new timer
    changed timer to a simple system timer.

--- PlutonEssentials/PlutonEssentials/PlutonEssentials.cs
+++ PlutonEssentials/PlutonEssentials/PlutonEssentials.cs
@@ -11,8 +11,8 @@ namespace PlutonEssentials
 		const string author = "Pluton Team";
 		const string version = "0.9.1";
 
-		public static ServerTimers Timers;
-		public static PlutonEssentials Instance;
+        private static Timer aTimer;
+        public static PlutonEssentials Instance;
 
 		public void On_ServerInit()
 		{
@@ -68,46 +68,36 @@ namespace PlutonEssentials
 				ConfigFile.Save();
 			}
 			IniParser GetConfig = Plugin.GetIni("PlutonEssentials");
-			Commands.Register(GetConfig.GetSetting("Commands", "ShowMyStats", "mystats")).setCallback("mystats");
-			Commands.Register(GetConfig.GetSetting("Commands", "ShowStatsOther", "statsof")).setCallback("statsof");
-			Commands.Register(GetConfig.GetSetting("Commands", "ShowLocation", "whereami")).setCallback("whereami");
-			Commands.Register(GetConfig.GetSetting("Commands", "ShowOnlinePlayers", "players")).setCallback("players");
-			Commands.Register(GetConfig.GetSetting("Commands", "Help", "help")).setCallback("help");
-			Commands.Register(GetConfig.GetSetting("Commands", "Commands", "commands")).setCallback("commands");
-			Commands.Register(GetConfig.GetSetting("Commands", "Description", "whatis")).setCallback("whatis");
-			Commands.Register(GetConfig.GetSetting("Commands", "Usage", "howto")).setCallback("howto");
-		}
-
-		public void ReloadTimers()
-		{
-			if (Timers != null)
-				Timers.Dispose();
-			IniParser ConfigFile = Plugin.GetIni("PlutonEssentials") ;
-			var broadcast = ConfigFile.GetSetting("Config", "broadcastInterval", "600000");
-			if (broadcast != null) {
-				double ads = Double.Parse(broadcast);
-
-				Timers = new ServerTimers(ads);
-				Timers.Start();
-			}
-		}
+			Commands.Register(GetConfig.GetSetting("Commands", "ShowMyStats", "mystats")).setCallback(mystats);
+			Commands.Register(GetConfig.GetSetting("Commands", "ShowStatsOther", "statsof")).setCallback(statsof);
+			Commands.Register(GetConfig.GetSetting("Commands", "ShowLocation", "whereami")).setCallback(whereami);
+			Commands.Register(GetConfig.GetSetting("Commands", "ShowOnlinePlayers", "players")).setCallback(players);
+			Commands.Register(GetConfig.GetSetting("Commands", "Help", "help")).setCallback(help);
+			Commands.Register(GetConfig.GetSetting("Commands", "Commands", "commands")).setCallback(commands);
+			Commands.Register(GetConfig.GetSetting("Commands", "Description", "whatis")).setCallback(whatis);
+			Commands.Register(GetConfig.GetSetting("Commands", "Usage", "howto")).setCallback(howto);
+            int broadcast_time = int.Parse(GetConfig.GetSetting("Config", "broadcastInterval", "600000"));
+            aTimer = new System.Timers.Timer(broadcast_time);
+            aTimer.Elapsed += Advertise;
+            aTimer.Enabled = true;
+        }
 
 		public void mystats(string[] args, Player player) {
 			PlayerStats stats = player.Stats;
-			player.Message(String.Format("You have {0} kills and {1} deaths!", stats.Kills, stats.Deaths));
-			player.Message(String.Format("You have taken {0} dmg, and caused {1} in total!", stats.TotalDamageTaken, stats.TotalDamageDone));
+			player.Message(string.Format("You have {0} kills and {1} deaths!", stats.Kills, stats.Deaths));
+			player.Message(string.Format("You have taken {0} dmg, and caused {1} in total!", stats.TotalDamageTaken, stats.TotalDamageDone));
 			return;
 		}
 
 		public void statsof(string[] args, Player player) {
-			Player pOther = Player.Find(String.Join(" ", args[0]));
+			Player pOther = Player.Find(string.Join(" ", args[0]));
 			if (pOther != null) {
 				PlayerStats stats2 = pOther.Stats;
-				player.Message(String.Format(pOther.Name + " has {0} kills and {1} deaths!", stats2.Kills, stats2.Deaths));
-				player.Message(String.Format(pOther.Name + " has taken {0} dmg, and caused {1} in total!", stats2.TotalDamageTaken, stats2.TotalDamageDone));
+				player.Message(string.Format(pOther.Name + " has {0} kills and {1} deaths!", stats2.Kills, stats2.Deaths));
+				player.Message(string.Format(pOther.Name + " has taken {0} dmg, and caused {1} in total!", stats2.TotalDamageTaken, stats2.TotalDamageDone));
 				return;
 			}
-			player.Message("Can't find player: " + String.Join(" ", args[0]));
+			player.Message("Can't find player: " + string.Join(" ", args[0]));
 			return;
 		}
 
@@ -119,7 +109,7 @@ namespace PlutonEssentials
 
 		public void players(string[] args, Player player)
 		{
-			string msg = Server.Players.Count == 1 ? "You are alone!" : String.Format ("There are: {0} players online!", Server.Players.Count);
+			string msg = Server.Players.Count == 1 ? "You are alone!" : string.Format ("There are: {0} players online!", Server.Players.Count);
 			player.Message (msg);
 			return;
 		}
@@ -142,7 +132,7 @@ namespace PlutonEssentials
 			foreach (ChatCommands cm in cc) {
 				list.AddRange(cm.getCommands());
 			}
-			player.Message(String.Join(", ", list.ToArray()));
+			player.Message(string.Join(", ", list.ToArray()));
 		}
 
 		public void whatis(string[] args, Player player)
@@ -159,7 +149,7 @@ namespace PlutonEssentials
 					list.AddRange(cm.getDescriptions(args[0]));
 				}
 				if (list.Count > 0)
-					player.Message(String.Join("\r\n", list.ToArray()));
+					player.Message(string.Join("\r\n", list.ToArray()));
 			}
 		}
 
@@ -177,51 +167,17 @@ namespace PlutonEssentials
 					list.AddRange(cm.getUsages(args[0]));
 				}
 				foreach (var item in list) {
-					player.Message(String.Format("/{0} {1}", args[0], item));
+					player.Message(string.Format("/{0} {1}", args[0], item));
 				}
 			}
 		}
 
-		public static void Advertise()
+        private static void Advertise(object source, ElapsedEventArgs e)
 		{
 			IniParser ConfigFile = Instance.Plugin.GetIni("PlutonEssentials") ;
 			foreach (string arg in ConfigFile.EnumSection("BroadcastMessages")) {
 				Instance.Server.Broadcast(ConfigFile.GetSetting("BroadcastMessages", arg));
 			}
-		}
-	}
-
-	public class ServerTimers
-	{
-		public readonly Timer _adstimer;
-
-		public ServerTimers(double ads)
-		{
-			_adstimer = new Timer(ads);
-
-			Debug.Log("Broadcast timer started!");
-			_adstimer.Elapsed += _adstimer_Elapsed;
-		}
-
-		public void Dispose()
-		{
-			Stop();
-			_adstimer.Dispose();
-		}
-
-		public void Start()
-		{
-			_adstimer.Start();
-		}
-
-		public void Stop()
-		{
-			_adstimer.Stop();
-		}
-
-		static void _adstimer_Elapsed(object sender, ElapsedEventArgs e)
-		{
-			PlutonEssentials.Advertise();
 		}
 	}
 }
